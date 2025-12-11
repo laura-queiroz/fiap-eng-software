e03fe9c2257a6ca11249e3de745fbd9bf0ff5998
Laura Nunes <laura.sa@youse.com.br>
2025-12-11 15:38:16 -0300

    add unit tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product must have a name" do
+    product = Product.new(description: "Test product", price: 10)
+    assert_not product.valid?
+  end
+
+  test "product must have a price" do
+    product = Product.new(name: "Pen", description: "Blue pen")
+    assert_not product.valid?
+  end
+
+  test "product is valid with name, description, and price" do
+    product = Product.new(name: "Pen", description: "Blue pen", price: 10)
+    assert product.valid?
+  end
 end
